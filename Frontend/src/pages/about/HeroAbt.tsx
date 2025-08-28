1192b6d8f98fdac4590d386072234386fc5b7dab
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-28 23:41:02 +0530

    pricing

--- Frontend/src/pages/about/HeroAbt.tsx
+++ Frontend/src/pages/about/HeroAbt.tsx
@@ -1,3 +1,52 @@
 export default function HeroAbt() {
-  return <div>HeroAbt</div>;
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2 text-center">
+          We pioneered the discount broking model in India. <br /> Now, we are
+          pioneering with our technology.
+        </h1>
+      </div>
+
+      <div className="container p-5 mt-5 mb-5 border-top">
+        <div className="row">
+          <div className="col-6">
+            <p className="text-muted ">
+              We kick-started operations on the 15th of August, 2010 with the
+              goal of breaking all barriers that traders and investors face in
+              India in terms of cost, support, and technology. We named the
+              company Zerodha, a combination of Zero and "Rodha", the Sanskrit
+              word for barrier. <br /> <br />
+              <br />
+              Today, our disruptive pricing models and in-house technology have
+              made us the biggest stock broker in India. <br /> <br />
+              <br />
+              Over 1.6+ crore clients place billions of orders every year
+              through our powerful ecosystem of investment platforms,
+              contributing over 15% of all Indian retail trading volumes.
+            </p>
+          </div>
+
+          <div className="col-6">
+            <p className="text-muted ">
+              In addition, we run a number of popular open online educational
+              and community initiatives to empower retail traders and investors.
+              <br />
+              <br /> <br />
+              <a href="https://rainmatter.com/" target="_blank">
+                Rainmatter
+              </a>
+              , our fintech fund and incubator, has invested in several fintech
+              startups with the goal of growing the Indian capital markets.{" "}
+              <br /> <br /> <br />
+              And yet, we are always up to something new every day. Catch up on
+              the latest updates on our blog or see what the media is saying
+              about us or learn more about our business and product
+              philosophies.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
