721cc168ce5cf754b40ab3b2f6ae9bfa3842928c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-28 23:58:34 +0530

    products

--- Frontend/src/pages/products/ProductsPage.tsx
+++ Frontend/src/pages/products/ProductsPage.tsx
@@ -2,14 +2,60 @@ import HeroTech from "./HeroTech";
 import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 import Universe from "./Universe";
+import kite from "../../assets/kite.png";
+import console from "../../assets/console.png";
+import coin from "../../assets/coin.png";
+import connect from "../../assets/kiteconnect.png";
+import varsity from "../../assets/varsity.png";
 
 export default function ProductsPage() {
   return (
     <>
       <HeroTech />
+      <LeftSection
+        imageURL={kite}
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageURL={console}
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
+      <LeftSection
+        imageURL={coin}
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageURL={connect}
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+      />
+      <LeftSection
+        imageURL={varsity}
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <h4 className="text-center text-muted mt-5 mb-5">
+        Want to know more about our technology stack? Check out the Zerodha.tech
+        blog.
+      </h4>
       <Universe />
-      <LeftSection />
-      <RightSection />
     </>
   );
 }
--- Frontend/src/pages/products/RightSection.tsx
+++ Frontend/src/pages/products/RightSection.tsx
@@ -1,3 +1,30 @@
-export default function RightSection() {
-  return <div>RightSection</div>;
+type RightSectionProps = {
+  imageURL: string;
+  productName: string;
+  productDescription: string;
+  learnMore: string;
+};
+
+export default function RightSection({
+  imageURL,
+  productName,
+  productDescription,
+  learnMore,
+}: RightSectionProps) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={learnMore}>Learn More</a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageURL} alt={productName} />
+        </div>
+      </div>
+    </div>
+  );
 }
