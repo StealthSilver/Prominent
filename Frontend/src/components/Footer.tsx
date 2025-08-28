09f43566e2cb142efa0a5e9f1ef9700a5218b9ad
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-28 11:49:54 +0530

    footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,3 +1,505 @@
+import logo from "../assets/logo.svg";
+import { Facebook, Instagram, Linkedin } from "lucide-react";
+import x from "../assets/icons8-x.svg";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="py-5 text-left border-top mt-5"
+      style={{
+        backgroundColor: "#f8f9fa",
+        paddingLeft: "300px",
+        paddingRight: "300px",
+      }}
+    >
+      <div className="row">
+        <div className="col mr-5">
+          <img src={logo} alt="logo" width="80%" />
+          <p className="text-muted mt-4">
+            © 2010 - 2025, Zerodha Broking Ltd. All rights reserved.
+          </p>
+          <hr />
+          <div className="text-muted d-flex align-items-center gap-3">
+            <img src={x} alt="x logo" width="25px" />
+            <Facebook />
+            <Instagram />
+            <Linkedin />
+          </div>
+        </div>
+        <div className="col text-muted ">
+          <h5 className="mb-4">Account</h5>
+          <a
+            href=""
+            className="no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Open demat account
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Minor demat account
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            NRI demat account
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Commodity
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Dematerialisation
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Fund transfer
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            MTF
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Referral program
+          </a>
+          <br />
+        </div>
+        <div className="col text-muted">
+          <h5 className="mb-4">Support</h5>
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Contact us
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Support portal
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            How to file a complaint?
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Status of your complaints
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{ textDecoration: "none", color: "gray" }}
+          >
+            Bulletin
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Circular
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Z-Connect blog
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Downloads
+          </a>
+          <br />
+        </div>
+        <div className="col text-muted">
+          <h5 className="mb-4">Company</h5>
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            About
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Philosophy
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Press & media
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{ textDecoration: "none", color: "gray" }}
+          >
+            Careers
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Zerodha Cares (CSR)
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{ textDecoration: "none", color: "gray" }}
+          >
+            Zerodha.tech
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Open source
+          </a>
+          <br />
+        </div>
+        <div className="col text-muted">
+          <h5 className="mb-4">Quick Links</h5>
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Upcoming IPOs
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Brokerage charges
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Market holidays
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Economic calendar
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Calculators
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Markets
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-gray-600 no-underline hover:text-gray-800"
+            style={{
+              textDecoration: "none",
+              color: "gray",
+              fontSize: "16px",
+              fontWeight: 200,
+              display: "block",
+            }}
+          >
+            Sectors
+          </a>
+          <br />
+        </div>
+      </div>
+      <div
+        className="mt-5 leading-relaxed space-y-3 text-muted"
+        style={{ fontSize: "12px" }}
+      >
+        <p>
+          Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration
+          no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha
+          Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading
+          through Zerodha Commodities Pvt. Ltd. MCX: 46025; SEBI Registration
+          no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154,
+          4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th
+          Phase, Bengaluru - 560078, Karnataka, India. For any complaints
+          pertaining to securities broking please write to
+          complaints@zerodha.com, for DP related to dp@zerodha.com. Please
+          ensure you carefully read the Risk Disclosure Document as prescribed
+          by SEBI | ICF
+        </p>
+        <p>
+          Procedure to file a complaint on SEBI SCORES: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances
+        </p>
+        <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+        <p>
+          Investments in securities market are subject to market risks; read all
+          the related documents carefully before investing.
+        </p>
+        <p>
+          Attention investors: 1) Stock brokers can accept securities as margins
+          from clients only by way of pledge in the depository system w.e.f
+          September 01, 2020. 2) Update your e-mail and phone number with your
+          stock broker / depository participant and receive OTP directly from
+          depository on your e-mail and/or mobile number to create pledge. 3)
+          Check your securities / MF / bonds in the consolidated account
+          statement issued by NSDL/CDSL every month.
+        </p>
+        <p>
+          India's largest broker based on networth as per NSE. NSE broker
+          factsheet
+        </p>
+        <p>
+          "Prevent unauthorised transactions in your account. Update your mobile
+          numbers/email IDs with your stock brokers. Receive information of your
+          transactions directly from Exchange on your mobile/email at the end of
+          the day. Issued in the interest of investors. KYC is one time exercise
+          while dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+          create a ticket here.
+        </p>
+      </div>
+    </div>
+  );
 }
