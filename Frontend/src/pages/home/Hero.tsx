284da1caea60128b03f01383f99b57aee93663f0
Silver <rajat.saraswat.0409@gmail.com>
2025-08-29 13:12:07 +0530

    Update Hero.tsx

--- Frontend/src/pages/home/Hero.tsx
+++ Frontend/src/pages/home/Hero.tsx
@@ -18,9 +18,9 @@ export default function Hero() {
         bonds, and more.
       </p>
 
-      <Button as="a" variant="primary" size="lg" className="mt-4 custom-hover">
-          <Link to="/signup">Sign up for free</Link>
-      </Button>
+     <Button as={Link} to="/signup" variant="primary" size="lg" className="mt-4 custom-hover text-white text-decoration-none">
+  Sign up for free
+</Button>
     </div>
   );
 }
