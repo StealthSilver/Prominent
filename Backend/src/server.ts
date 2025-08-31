b53fe71fbb2e51ef8d36b1ecf15caf87fe4d4f7d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-31 07:59:40 +0530

    backend setup

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONG_URI || "", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello, Prominent Backend!");
